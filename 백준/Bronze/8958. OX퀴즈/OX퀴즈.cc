f648ce0cd089f22e7b79c2b37d24fddf9d9a09ed
Hyuna Kim <60914488+Hyuna-319@users.noreply.github.com>
2025-03-30 18:07:15 +0900

    [Bronze II] Title: OX퀴즈, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.cc
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.cc
@@ -1,42 +1,34 @@
 #include <iostream>
-#include <vector>
 #include <string>
 
 int main() {
+
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
-
-    int n ;
-    std::cin >> n;
     
+    int n;
+    std::cin >> n;
+
     while (n--) {
         std::string s;
         std::cin >> s;
-
-        std::vector<char> v;
-        for(int i = 0; i < s.length(); i++) {
-            v.push_back(s[i]);
-        }
-
-        int sum = 0;
+        
         int score = 0;
+        int sum = 0;
 
-        for (int i=0; i < v.size(); i++) {
-            if (v[i] == 'O'){
+        for (int i=0; i < s.length(); i++) {
+            if (s[i] == 'O') {
                 score ++ ;
                 sum += score;
-
-            } else {
+            }
+            else {
                 score = 0;
             }
         }
-
+        
         std::cout << sum << std::endl;
-  
 
+        
     }
-
     return 0;
 }
-    
-    
