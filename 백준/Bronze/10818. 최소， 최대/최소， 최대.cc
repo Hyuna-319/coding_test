233f226dea6688559c5107f17abbca8c1d0d4553
Hyuna Kim <60914488+Hyuna-319@users.noreply.github.com>
2025-03-30 16:36:33 +0900

    [Bronze III] Title: 최소, 최대, Time: 104 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -1,25 +1,27 @@
 #include <iostream>
+#include <vector>
 
 int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
     int n;
     std::cin >> n;
 
-    int arr[1000000];
+    std::vector<int> v(n);
 
-    for (int i = 0; i <n; i++){
-        std::cin >> arr[i];
-
-    }
-    int min_val = arr[0];
-    int max_val = arr[0];
-
-    for (int i = 1; i< n; i++){
-        if (arr[i] < min_val) min_val = arr[i];
-        if (arr[i] > max_val) max_val = arr[i];
+    for (int i=0; i <n; i++){
+        std::cin >> v[i];
     }
 
+    int min_val = v[0];
+    int max_val = v[0];
+
+    for (int i=1; i <n; i++){
+        if (v[i]< min_val) min_val = v[i];
+        if (v[i]> max_val) max_val = v[i];
+    }
+    
     std::cout << min_val << " " << max_val << std::endl;
     return 0;
-
-
 }
