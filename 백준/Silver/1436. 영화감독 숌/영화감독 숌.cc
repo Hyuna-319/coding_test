a74123ce1926b524986a4c0e812aaae4481495b4
Hyuna Kim <60914488+Hyuna-319@users.noreply.github.com>
2025-03-31 23:01:38 +0900

    [Silver V] Title: 영화감독 숌, Time: 60 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <set>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <functional>
+
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    int n;
+    std::cin >> n;
+    
+    int count =0;
+    int num = 666;
+
+    while(true) {
+        if (std::to_string(num).find("666") != std::string::npos){
+            count ++;
+
+            if (count == n) break;
+
+        }
+        num ++;
+
+
+    }
+    std::cout << num << '\n';
+
+    return 0;
+}
