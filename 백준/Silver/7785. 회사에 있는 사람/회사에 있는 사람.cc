b83c0fae5b17b111d6dc55aadee6bcb7072296a3
Hyuna Kim <60914488+Hyuna-319@users.noreply.github.com>
2025-04-04 18:37:54 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 80 ms, Memory: 12984 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
@@ -1,40 +1,31 @@
-#include <iostream>
-#include <vector>
-#include <tuple>
-#include <set>
-#include <string>
-#include <algorithm>
-#include <functional>
+#include <iostream> //표준 입출력
+#include <vector> // 동적배열
+#include <map> // key → value 형태의 자료구조
+#include <string> // 문자열
+#include <algorithm> //정렬, 최대최소(max_element)
+#include <functional> //비교 (greater<>)
 
 
-int main(){
+
+int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
 
-    int n;
-    std::cin >> n;
+    int N;
+    std::cin >> N;
 
-    std::set<std::string> office;
+    std::map<std::string, std::string, std::greater<>> office;
 
-    for (int i=0; i<n; i++){
+    for(int i=0; i<N; i++){
         std::string name, work;
-        std::cin >> name >> work ;
+        std::cin >> name >> work;
+        office[name] = work;
+    }
 
-        if (work == "enter") {
-            office.insert(name);
-        } else {
-            office.erase(name);
-        }
+    for (auto [name, work]: office){
+        if (work == "enter") std::cout << name << '\n';
     }
-    
-    std::vector<std::string> answer(office.begin(), office.end());
-    std::sort(answer.rbegin(), answer.rend());
-    
-    for (int i=0; i < answer.size(); i++){
-        std::cout << answer[i] << '\n';
-    }
-      
+
 
     return 0;
-
 }
