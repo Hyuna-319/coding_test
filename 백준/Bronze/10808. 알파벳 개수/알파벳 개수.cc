a6a216b2868b72e308473136ef442db6005444f5
Hyuna Kim <60914488+Hyuna-319@users.noreply.github.com>
2025-04-16 01:13:04 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,28 +1,22 @@
-#include <iostream> //표준 입출력
-#include <vector> // 동적배열
-#include <map> // key → value 형태의 자료구조
-#include <string> // 문자열
-#include <algorithm> //정렬, 최대최소(max_element)
-#include <functional> //비교 (greater<>)
+#include <bits/stdc++.h>
 
-#include <unordered_map>
+using namespace std;
 
+string s;
 
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
+    vector<int> count(26,0);
 
-    std::string s;
-    std::cin >> s;
+    cin >> s;
 
-    std::vector<int> count(26,0);
-
-    for (char c:s){
-        count[c -'a']++;
+    for(int ch : s){
+        count[ch-'a']++;
     }
 
-    for (int i=0; i<26; i++){
-        std::cout << count[i] << ' ';
+    for (int i : count){
+        cout << i << ' ';
     }
 
 
